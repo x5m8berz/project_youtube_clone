38069bcb854a9ea5e68c0eea1fef39ef5adfe560
Hanrocky <hanrocky.halim@gmail.com>
2023-01-28 14:14:17 +0700

    Create the route

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,7 +1,18 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
 
 const App = () => {
-  return <div>App</div>;
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: "#000" }}>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Feed />} />
+        <Route path="/video/:id" element={<VideoDetail />} />
+        <Route path="/channel/:id" element={<ChannelDetail />} />
+        <Route path="/search/:searchTerm" element={<SearchFeed />} />
+      </Routes>
+    </Box>
+  </BrowserRouter>;
 };
 
 export default App;
