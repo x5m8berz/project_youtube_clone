e4ab8ff2445c0b5b2a2f5d5188963aded7a5d926
Hanrocky <hanrocky.halim@gmail.com>
2023-01-29 19:00:49 +0700

    Finished search bar

--- Client/src/components/SearchBar.jsx
+++ Client/src/components/SearchBar.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Paper, IconButton } from "@mui/material";
-import { SearchIcon } from "@mui/icons-material";
+import { Search } from "@mui/icons-material";
 
 const SearchBar = () => {
   return (
@@ -22,8 +22,8 @@ const SearchBar = () => {
         value=""
         onChange={() => {}}
       />
-      <IconButton>
-        <SearchIcon />
+      <IconButton type="submit" sx={{ p:'10px', color:'red'}}>
+        <Search />
       </IconButton>
     </Paper>
   );
