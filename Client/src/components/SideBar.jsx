a519c08cccfc16595dea0c66d410f9f4b0e96f05
Hanrocky <hanrocky.halim@gmail.com>
2023-01-30 12:45:51 +0700

    Finished side bar

--- Client/src/components/SideBar.jsx
+++ Client/src/components/SideBar.jsx
@@ -18,11 +18,23 @@ const SideBar = () => (
         className="category-btn"
         style={{
           background: category.name === selectedCategory && "#FC1503",
-          color: 'white'
+          color: "white",
         }}
+        key={category.name}
       >
-        <span>{category.icon}</span>
-        <span>{category.name}</span>
+        <span
+          style={{
+            color: category.name === selectedCategory ? "white" : "red",
+            marginRight: "15px",
+          }}
+        >
+          {category.icon}
+        </span>
+        <span
+            style={{
+                opacity: category.name === selectedCategory ? '1' : '0.8'
+            }}
+        >{category.name}</span>
       </button>
     ))}
   </Stack>
