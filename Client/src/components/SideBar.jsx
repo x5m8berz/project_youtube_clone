af3d60556b3a88cecd6a6c395998f6a5c0d4bb21
Hanrocky <hanrocky.halim@gmail.com>
2023-01-30 12:14:04 +0700

    Added SideBar

---
+++ Client/src/components/SideBar.jsx
@@ -0,0 +1,17 @@
+import { Stack } from "@mui/material";
+
+import { categories } from "../utils/constant";
+
+const SideBar = () => (
+     <Stack direction="row"
+        sx={{
+            overflowY: "auto",
+            height: { sx:'auto', md: '95%'},
+            flexDirection: { md:'column'}
+        }}
+     >
+    
+    </Stack>
+);
+
+export default SideBar;
