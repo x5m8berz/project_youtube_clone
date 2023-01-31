15bcbfda4bc8e52833140b8ddb58b664bfff788e
Hanrocky <hanrocky.halim@gmail.com>
2023-01-31 22:29:34 +0700

    Added VideoDetail page

--- client/src/components/SearchFeed.jsx
+++ client/src/components/SearchFeed.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { Box, Typography } from "@mui/material";
+import { useParams } from "react-router-dom";
+
+import { fetchFromAPI } from "../utils/FetchFromAPI";
+import { Videos } from "./";
 
 const SearchFeed = () => {
-  return <div>SearchFeed</div>;
+
+  const [videos, setVideos] = useState([]);
+  const { searchTerm } = useParams()
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`).then((data) =>
+      setVideos(data.items)
+    );
+  }, [searchTerm]);
+
+  return (
+    <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+      <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" , ml: '100px'}}>
+        Search Result For : <span style={{ color: "#F31503" }}>{searchTerm}</span> videos
+      </Typography>
+      <Box display="flex" p="2" >
+        <Box sx={{ mr: { sm: "100px" } }} />
+        <Videos videos={videos} />
+      </Box>
+    </Box>
+  );
 };
 
 export default SearchFeed;
