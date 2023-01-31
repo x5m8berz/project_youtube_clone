441247001e5f3d140cf3ca955376e3bb47aa72f3
Hanrocky <hanrocky.halim@gmail.com>
2023-02-01 00:08:42 +0700

    Finish all feature

--- client/src/components/ChannelDetail.jsx
+++ client/src/components/ChannelDetail.jsx
@@ -33,7 +33,7 @@ const ChannelDetail = () => {
         <ChannelCard channelDetail={channelDetail} marginTop="-110px" />
       </Box>
       <Box display="flex" p="2">
-        <Box sx={{ mr: { sm: "100px" } }} />
+        {/* <Box sx={{ mr: { sm: "100px" } }} /> */}
         <Videos videos={videos} />
       </Box>
     </Box>
